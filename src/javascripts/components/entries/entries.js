e500d0e0936f72be6c8fe287c73ccc5700526dad
Heath JH Moore <heathjhmoore@gmail.com>
2019-06-10 19:53:09 -0500

    wip

--- src/javascripts/components/entries/entries.js
+++ src/javascripts/components/entries/entries.js
@@ -2,9 +2,11 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import $ from 'jquery';
+// import moment from 'moment';
 
 import util from '../../helpers/util';
 import entriesData from '../../helpers/data/entriesData';
+// import moment = require('moment');
 
 const entriesBuilder = () => {
   // below jquery selectors will be removed after testing branch
@@ -22,8 +24,10 @@ const entriesBuilder = () => {
         domString += `<h4>${entry.date}</h4>`;
         domString += `<p>${entry.entry}</p>`;
         domString += '<div class="d-flex justify-content-end">';
+        domString += '<button class="btn btn-primary col-2 saveEntryButton hide">Save</button>';
         domString += '<button class="btn btn-primary col-2 editEntryButton">Edit</button>';
         domString += '<button class="btn btn-danger col-2 deleteEntryButton">Delete</button>';
+        domString += '<button class="btn btn-danger col-2 cancelEntryButton hide">Cancel</button>';
         domString += '</div>';
         domString += '</div>';
         domString += '</div>';
@@ -40,13 +44,17 @@ const updateEntryToDom = (e) => {
   console.error(targetedEntry);
   const title = $(targetedEntry).find('h2');
   // const date = $(targetedEntry).find('h4');
-  // const entry = $(targetedEntry).find('p');
+  const entry = $(targetedEntry).find('p');
   title.addClass('editable');
   title.attr('contenteditable', 'true');
-  // $(targetedEntry).find('h4').addClass('editable');
-  // $(targetedEntry).find('h4').attr('contenteditable', 'true');
-  // $(targetedEntry).find('p').addClass('editable');
-  // $(targetedEntry).find('p').attr('contenteditable', 'true');
+  // date.innerHTML = moment.creea
+  entry.find('p').addClass('editable');
+  entry.find('p').attr('contenteditable', 'true');
+  $(targetedEntry).find('.saveEntryButton').removeClass('hide');
+  $(targetedEntry).find('.cancelEntryButton').removeClass('hide');
+  $(targetedEntry).find('.editEntryButton').addClass('hide');
+  $(targetedEntry).find('.deleteEntryButton').addClass('hide');
+  // date.innerHTML = moment().format()
   title.focus();
 };
 
