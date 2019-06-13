09cf4f165343c76d526d5d4acf8305251ed32db6
Emily DeWitt <emilykdewitt@gmail.com>
2019-06-12 19:21:20 -0500

    Fixed smash issue

--- src/javascripts/helpers/smash.js
+++ src/javascripts/helpers/smash.js
@@ -1,13 +1,14 @@
 const messagesWithUserInfo = (messages, users) => {
   const messagesWithUserInfoArray = [];
   messages.forEach((message) => {
+    const newMessage = message;
     users.forEach((user) => {
       if (message.uid === user.uid) {
-        message.userName = user.name;
-        message.userImage = user.image;
+        newMessage.userName = user.name;
+        newMessage.userImage = user.image;
       }
     });
-    messagesWithUserInfoArray.push(message);
+    messagesWithUserInfoArray.push(newMessage);
   });
   return messagesWithUserInfoArray;
 };
