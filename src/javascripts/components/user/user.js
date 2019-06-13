2a7a216c87bdcf7066e81f5eecf6ef1ef9cf3419
Greg Stephen <GregAStephen@gmail.com>
2019-06-12 20:29:26 -0500

    changes the username

--- src/javascripts/components/user/user.js
+++ src/javascripts/components/user/user.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import $ from 'jquery';
-import userData from '../../helpers/data/userData';
+import userData from '../../helpers/data/usersData';
 import eventsData from '../../helpers/data/eventsData';
 import articlesData from '../../helpers/data/articlesData';
 import entriesData from '../../helpers/data/entriesData';
@@ -12,10 +12,6 @@ const displayNameInNavbar = (userId) => {
   userData.getUserInfoByUserId(userId)
     .then((user) => {
       $('.user-button').html(`<img id="user-navbar-avatar" src="${user[0].image}"></img> ${user[0].name}`);
-      $('#edit-profile-nav-button').attr('data-userobjectid', `${user[0].id}`);
-      $('#edit-profile-nav-button').attr('data-userid', `${user[0].uid}`);
-      $('#edit-profile-nav-button').attr('data-username', `${user[0].name}`);
-      $('#edit-profile-nav-button').attr('data-avatar', `${user[0].image}`);
     })
     .catch(err => console.error('can not display name in navbar', err));
 };
@@ -58,29 +54,55 @@ const deleteProfile = (e) => {
   logoutUser();
 };
 
-const buildEditProfileModal = (e) => {
-  const button = $(e.target);
-  const userObjectId = button.data('userobjectid');
-  const userId = button.data('userid');
-  const username = button.data('username');
-  let domstring = '';
-  domstring += '<div class="input-group mb-3">';
-  domstring += '<div class="input-group-prepend">';
-  domstring += '<span class="input-group-text" id="basic-addon1">Username</span>';
-  domstring += '</div>';
-  domstring += `<input type="text" class="form-control" value="${username}" aria-label="Username" aria-describedby="basic-addon1">`;
-  domstring += '</div>';
-  domstring += `<button id="delete-profile" class="btn btn-danger" data-userobjectid="${userObjectId}"`;
-  domstring += `data-userid="${userId}" data-toggle="modal" data-target="#areYouSureDeleteModal">Delete Profile</button>`;
-  util.printToDom('edit-profile-modal-body', domstring);
-  $('#are-you-sure-delete').attr('data-userobjectid', `${userObjectId}`);
-  $('#editProfileModal').modal('show');
+const buildEditProfileModal = () => {
+  const usersId = firebase.auth().currentUser.uid;
+  userData.getUserInfoByUserId(usersId)
+    .then((userObj) => {
+      let domstring = '';
+      domstring += `<div id="userObjDiv" class="container" data-userobjectid="${userObj[0].id}">`;
+      domstring += '<div id="avatar-in-modal" class="row justify-content-center">';
+      domstring += `<img id="edit-avatar-modal" class="col-12 mb-4" src="${userObj[0].image}"></img>`;
+      domstring += '<button class="btn btn-info col-auto">Change Avatar</button>';
+      domstring += '</div>';
+      domstring += '<div class="input-group mb-3">';
+      domstring += '<div class="input-group-prepend">';
+      domstring += '<span class="input-group-text" id="basic-addon1">Username</span>';
+      domstring += '</div>';
+      domstring += `<input type="text" id="update-username" class="form-control" value="${userObj[0].name}" aria-label="Username" aria-describedby="basic-addon1">`;
+      domstring += '</div>';
+      domstring += `<button id="delete-profile" class="btn btn-danger col-12" data-userobjectid="${userObj[0].id}"`;
+      domstring += `data-userid="${userObj[0].uid}" data-toggle="modal" data-target="#areYouSureDeleteModal">Delete Profile</button>`;
+      domstring += '</div>';
+      util.printToDom('edit-profile-modal-body', domstring);
+      $('#are-you-sure-delete').attr('data-userobjectid', `${userObj[0].id}`);
+      $('#editProfileModal').modal('show');
+    })
+    .catch();
+};
+
+const updatesChanges = () => {
+  const userObjId = $('#userObjDiv').data('userobjectid');
+  const updatedUsername = $('#update-username').val();
+  const updatedAvatar = $('#edit-avatar-modal').attr('src');
+  const userId = firebase.auth().currentUser.uid;
+  const updatedUserInfo = {
+    name: updatedUsername,
+    image: updatedAvatar,
+    uid: userId,
+  };
+  userData.editUsersInfo(userObjId, updatedUserInfo)
+    .then(() => {
+      $('#editProfileModal').modal('hide');
+      displayNameInNavbar(userId);
+    })
+    .catch();
 };
 
 const userEventHandlers = () => {
   $('#logout-nav-button').on('click', logoutUser);
   $('#edit-profile-nav-button').on('click', buildEditProfileModal);
   $('#are-you-sure-delete').on('click', deleteProfile);
+  $('#user-save-updates').on('click', updatesChanges);
 };
 
 export default { displayNameInNavbar, userEventHandlers };
